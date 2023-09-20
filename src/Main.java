5e8a3893fd0fbddc13deaec9b98aa58a5bbf9ce8
MattiYaqoob3 <matti.al-hayali@studerande.plushogskolan.se>
2023-09-20 12:54:58 +0200

    we get a uppadet

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
 import java.lang.module.FindException;
+import java.net.SocketOption;
+import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+
+
+      /*  Import
+
+
+        int antal;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Mata in hur många anställda som du vill betala ut löner till");
+        antal = input.nextInt();
+        int [] mony = new int [antal];
+        double val = 0.54;
+        for (int index = 0; index< antal; index++){
+            System.out.println("Mata in anställd "+(index+1));
+            mony[index] = input.nextInt();
+        }
+        input.close();
+
+        for (int index = 0; index< antal; index++){
+            System.out.println(mony[index]*val);
+        }
+
+       */
+
+
+
         /*
       int not;
       Scanner input = new Scanner(System.in);
@@ -19,22 +48,97 @@ public class Main {
           System.out.println(names[index]);
       }
 
-         */
+
 
       int antal;
       Scanner input = new Scanner(System.in);
       System.out.println("antal anställda som man vill betala ut löner till");
       antal = input.nextInt();
       int [] mony = new int[antal];
-      double val;
+      double val = 0.60;
       for (int index = 0; index< antal; index++){
           System.out.println("Mata in första blöp"+ (index + 1));
           mony[index] = input.nextInt();
         }
       input.close();
       for (int index = 0; index< antal; index++){
-          val = mony[index] * 0.60;
-          System.out.println(mony[index]+val);
+         // val = mony[index] * 0.60;
+          System.out.println(mony[index]* val);
+
+
       }
+
+         */
+        /*
+        int[] Cars = {20,30,49,66};
+        for (int index = 0; index < Cars.length; index++){
+            System.out.println(Cars[index]);
+        }
+
+
+
+        String[] cars = {"volvo", "bmw", "marcedx"};
+        for (int index = 0; index < cars.length; index++){
+            System.out.println(cars[index]);
+        }
+
+        String [] cars = {"volvo", "BMW", "ciat"};
+        cars[0]= "matti";
+       for (String i: cars){
+           System.out.println(i);
+       }
+
+
+
+        String[] cars = {"volvo","Bmw","Ford"};
+        for (String i : cars){
+            System.out.println(i);
+        }
+
+
+        for (int index = 0; index< cars.length; index++){
+            System.out.println(cars[index]);
+        }
+
+
+
+
+         */
+        /*
+
+        int antal;
+        Scanner input = new Scanner(System.in);
+        System.out.println("antal anställda som man vill betala ut löner till");
+        antal = input.nextInt();
+
+        int [] mony = new int[antal];
+        double val;
+        for (int index = 0; index< antal; index++){
+            System.out.println("Mata in första blöp"+ (index + 1));
+            mony[index] = input.nextInt();
+        }
+        input.close();
+        for (int index = 0; index< antal; index++){
+            val = mony[index] * 0.7;
+            System.out.println(val);
+        }
+
+
+        int number;
+        double bank = 5000;
+        System.out.println("put your number");
+        Scanner input = new Scanner(System.in);
+        number = input.nextInt();
+        number = input.nextInt();
+        double rezolten = (bank - number);
+        double result = number - bank;
+        System.out.println(result);
+
+         */
+
+
+
+
+
     }
 }
